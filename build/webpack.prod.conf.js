18d029b18cb4e006fec64b1ceb97c5b43ab4e693
wang_dgang <wdg404@163.com>
2019-01-05 18:19:58 +0800

    library target

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,10 +10,15 @@ function resolve(dir) {
 
 module.exports = merge(baseWebpackConfig, {
   mode: 'production',
-  entry: resolve('src/index.js'),
+  entry: {
+    'vue-carousel-card': resolve('src/index.js')
+  },
   output: {
-    filename: 'vue-carousel-card.min.js',
-    path: resolve('lib')
+    path: resolve('lib'),
+    filename: '[name].js',
+    library: 'VueCarouselCard', // 指定的就是你使用require时的模块名
+    libraryTarget: 'umd', // 指定输出格式
+    umdNamedDefine: true
   },
   plugins: [
     new UglifyJsPlugin({
